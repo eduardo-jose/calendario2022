8fc408477a47dea3d42f33a495c76b7920136a0f
Eduardo <contreras.eduardo.251099@gmail.com>
2022-02-24 20:53:08 -0300

    agregue onload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// function entrar(){
+//     alert("Esta es un pagina que creer con la intención de practicar lo que iba aprendiendo. En el repositorio hay un archivo README.md con mi instagram, si notas algo que pueda mejorar escríbeme por ahi. Gracias!");
+// }
+
 document.getElementById('titulo').ondblclick = nuevoTitulo;
 
 function nuevoTitulo(){
-    document.getElementById('titulo').innerHTML = 'Creado por EDU';
+    document.getElementById('titulo').innerHTML = 'Creado por EDUARDO';
 }
